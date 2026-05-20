bebb7e4d4d15b454a4aea7a602e36a04af9aff77
Your Name <your.email@csiro.au>
2026-05-20 12:14:25 +0930

    This is a ot of work and the data doesnt go back far enough, if I need to come back to this I will but I will leav it for now. keep to the plots I have already made with ABAERES data on cropping area per state and crop type not spatial data but it has the crop type whcih is improtant.

--- land use change maps.R
+++ land use change maps.R
@@ -26,67 +26,58 @@ lu1996 <- rast("1993_02/LanduseofAustralia_93-02v3/grids/lu96v3")
 lu2000 <- rast("1993_02/LanduseofAustralia_93-02v3/grids/lu00v3")
 
 
-#Check
-plot(lu2020)
+################################################################################
+### 2020 #####
+getwd()
+lookup2020 <- read.csv("2020_21/NLUM_v7_250_ALUMV8_2020_21_alb.csv")
+names(lookup2020)
+head(lookup2020)
+
+table(lookup2020$SIMP)
+table(lookup2020$AGIND)
 
 
-
-# based on ALUM = Australian Land Use and Management classification
-reclass_fun <- function(x) {
-  ifelse(
-    x >= 200 & x < 300,
-    1,
-    # Cropping
-    ifelse(
-      x >= 300 & x < 400,
-      2,
-      # Grazing modified
-      ifelse(
-        x >= 100 & x < 200,
-        3,
-        # Grazing natural
-        ifelse(
-          x >= 500 & x < 600,
-          4,
-          # Horticulture
-          ifelse(
-            x >= 600 & x < 700,
-            5,
-            # Intensive
-            6
-          )
-        )
-      )
-    )
-  )
-}
-
-lu_all_simple <- list(
-  lu1992 = app(lu1992, reclass_fun),
-  lu1996 = app(lu1996, reclass_fun),
-  lu2000 = app(lu2000, reclass_fun),
-  lu2005 = app(lu2005, reclass_fun),
-  lu2010 = app(lu2010, reclass_fun),
-  lu2015 = app(lu2015, reclass_fun),
-  lu2020 = app(lu2020, reclass_fun)
+lookup2020$final_class <- match(
+  lookup2020$AGIND,
+  c("Cropping",
+    "Grazing modified pastures",
+    "Grazing native vegetation",
+    "Horticulture",
+    "Intensive plant and animal industries",
+    "Not agricultural industry")
 )
 
 
-lu2020_simple <- app(lu2020, reclass_fun)
+rcl_2020 <- as.matrix(lookup2020[, c("Value", "final_class")])
+lu2020_simple <- classify(lu2020, rcl_2020)
+
+names(lu2020_simple)
+
 
 ## Plot helpers
 
+
+
+
 cols <- c(
-  "#8c8c8c",  # Grazing natural (grey)
-  "#c7e75f",  # Grazing modified (light green)
-  "#7a9a01",  # Cropping (olive green)
-  "#e31a1c",  # Horticulture (red)
-  "#4fc3dc",  # Intensive (blue)
-  "#d9d9d9"   # Other (light grey)
+  "#7a9a01",  # 1 Cropping (olive)
+  "#d9ef8b",  # 2 Grazing modified (pale yellow-green)
+  "#e3ded6",  # 3 Grazing native (lighter warm grey ✅)
+  "#e31a1c",  # 4 Horticulture (red)
+  "#6ecae3",  # 5 Intensive (soft blue)
+  "#ffffff"   # 6 Other (pure white ✅)
 )
 
 
 
+
+
+
+
+
+
+par(mar = c(3, 6, 3, 2))  
+# bottom, left, top, right
 
 
 
@@ -97,18 +88,22 @@ plot(lu2020_simple,
      box = FALSE,
      main = "Land Use Australia 2020–21")
 
-legend("bottomleft",
-       legend = c("Grazing native vegetation",
+legend("left",
+       legend = c("Cropping",
                   "Grazing modified pastures",
-                  "Cropping",
+                  "Grazing native vegetation",
                   "Horticulture",
                   "Intensive industries",
                   "Other uses"),
        fill = cols,
        bty = "n",
-       cex = 0.8)
+       cex = 0.9,
+       xpd = NA)   # allows drawing outside plot area
 
 
 
 
-#Something looks wrong there should be no cropping in the center
+#ok thats got it I think.
+## This may be a messy process 
+#I will need to use the lookup table in each downlaod folder and assign the classes
+# there is s folder called maps which have png for the maps I want to make to compare to.
